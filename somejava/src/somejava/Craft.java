f2254d07645678040f8a0a33e28f3072d267e05e
kmuzain <muzaink06@gmail.com>
2022-09-05 22:42:39 -0400

    some commit 2

--- somejava/src/somejava/Craft.java
+++ somejava/src/somejava/Craft.java
@@ -3,8 +3,8 @@ package somejava;
 public class Craft {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Some Java code");
+		System.out.println("Hello this is from craft");
 	}
 
 }
